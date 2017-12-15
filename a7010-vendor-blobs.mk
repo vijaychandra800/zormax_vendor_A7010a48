2430676c55ead457ebdd4ba028dad7ba2110b6e9
root <root@uc.c.lively-goal-180309.internal>
2017-12-15 09:19:44 +0000

    first commit

---
+++ a7010-vendor-blobs.mk
@@ -0,0 +1,29 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/app,system/app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/priv-app,system/priv-app)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/bin,system/vendor/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/lib,system/vendor/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/lib64,system/vendor/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/lib64,system/lib64)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/etc,system/vendor/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/firmware,system/vendor/firmware)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/xbin,system/xbin)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/vendor/xbin,system/vendor/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/mtk/a7010/thh,system/thh)
